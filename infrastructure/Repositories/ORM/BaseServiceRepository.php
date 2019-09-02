d5b80dae1f4835accdc970d219c8d9536ba8eaf2
Andriy D <andriy.osf@gmail.com>
2019-09-02 16:26:51 +0300

    Extended base repository class

--- infrastructure/Repositories/ORM/BaseServiceRepository.php
+++ infrastructure/Repositories/ORM/BaseServiceRepository.php
@@ -5,12 +5,55 @@ namespace Infrastructure\Repositories\ORM;
 
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Infrastructure\Models\ArraySerializable;
+use Infrastructure\Models\Collection;
+use Infrastructure\Models\CollectionWalk;
 
-class BaseServiceRepository extends ServiceEntityRepository
+abstract class BaseServiceRepository extends ServiceEntityRepository
 {
     public function save($entity)
     {
         $this->_em->persist($entity);
+        $this->_em->flush();
+    }
+
+    public function delete($entity)
+    {
+        $this->_em->remove($entity);
+        $this->_em->flush();
+    }
+
+    public function persist($entity)
+    {
+        $this->_em->persist($entity);
+    }
+
+    public function remove($entity)
+    {
+        $this->_em->remove($entity);
+    }
+
+    public function flush($entity = null)
+    {
+        $this->_em->flush($entity);
+    }
+
+    public function bathSave(Collection $entities)
+    {
+        $entities->walk(new class($this->_em) implements CollectionWalk {
+            private $entityManager;
+            public function __construct(EntityManagerInterface $entityManager)
+            {
+                $this->entityManager = $entityManager;
+            }
+
+            public function invoke(ArraySerializable $model, $key): void
+            {
+                $this->entityManager->persist($model);
+            }
+        });
+
         $this->_em->flush();
     }
 }
--- infrastructure/Repositories/ORM/RepositoryInterface.php
+++ infrastructure/Repositories/ORM/RepositoryInterface.php
@@ -2,11 +2,21 @@
 
 namespace Infrastructure\Repositories\ORM;
 
+use Infrastructure\Models\Collection;
+
 interface RepositoryInterface
 {
     public function find($id, $lockMode = null, $lockVersion = null);
     public function findOneBy(array $criteria, array $orderBy = null);
     public function findAll();
     public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null);
+
     public function save($entity);
+    public function delete($entity);
+
+    public function persist($entity);
+    public function remove($entity);
+    public function flush($entity);
+
+    public function bathSave(Collection $entities);
 }
